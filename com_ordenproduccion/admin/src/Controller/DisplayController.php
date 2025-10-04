064cc70546fb9c54691019967487689910c4c159
plgmgua <plgmgua@gmail.com>
2025-10-04 14:05:57 -0600

    fix: Add missing DisplayController and index.php files
    - Added admin/src/Controller/DisplayController.php with default view 'dashboard'
    - Added site/src/Controller/DisplayController.php with default view 'webhook'
    - Added admin/index.php and site/index.php for security
    - These files are required for Joomla component routing
    - Fixes '404 Invalid controller class: display' error
    - Version bump to 1.2.1 for missing controller fix

---
+++ com_ordenproduccion/admin/src/Controller/DisplayController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Grimpsa\Component\Ordenproduccion\Administrator\Controller;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Controller\BaseController;
+
+/**
+ * Display controller for com_ordenproduccion
+ *
+ * @since  1.0.0
+ */
+class DisplayController extends BaseController
+{
+    /**
+     * The default view for the display method.
+     *
+     * @var    string
+     * @since  1.0.0
+     */
+    protected $default_view = 'dashboard';
+}
