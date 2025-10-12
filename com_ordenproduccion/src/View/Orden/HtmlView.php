31c2efe73bf7853f2effd623a751980506098728
plgmgua <plgmgua@gmail.com>
2025-10-11 20:45:15 -0600

    bump: version to 2.8.1-STABLE

--- com_ordenproduccion/src/View/Orden/HtmlView.php
+++ com_ordenproduccion/src/View/Orden/HtmlView.php
@@ -457,6 +457,6 @@ class HtmlView extends BaseHtmlView
     public function getComponentVersion()
     {
         // Hardcoded version - update this with each release
-        return '2.8.0-STABLE';
+        return '2.8.1-STABLE';
     }
 }
