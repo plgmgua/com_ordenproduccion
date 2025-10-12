0525130154f2e8ba8c928241556878404420ebdb
plgmgua <plgmgua@gmail.com>
2025-10-11 21:02:25 -0600

    bump: version to 2.9.0-STABLE (major debugging feature)

--- com_ordenproduccion/src/View/Orden/HtmlView.php
+++ com_ordenproduccion/src/View/Orden/HtmlView.php
@@ -457,6 +457,6 @@ class HtmlView extends BaseHtmlView
     public function getComponentVersion()
     {
         // Hardcoded version - update this with each release
-        return '2.8.2-STABLE';
+        return '2.9.0-STABLE';
     }
 }
