06c9e26d3c6909fb369f2a4abe7a6123eb822e20
plgmgua <plgmgua@gmail.com>
2025-10-12 10:08:46 -0600

    bump: version to 3.1.0-STABLE (administracion dashboard)

--- com_ordenproduccion/src/View/Orden/HtmlView.php
+++ com_ordenproduccion/src/View/Orden/HtmlView.php
@@ -457,6 +457,6 @@ class HtmlView extends BaseHtmlView
     public function getComponentVersion()
     {
         // Hardcoded version - update this with each release
-        return '3.0.2-STABLE';
+        return '3.1.0-STABLE';
     }
 }
