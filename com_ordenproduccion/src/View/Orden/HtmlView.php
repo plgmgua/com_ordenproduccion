11adcd528034cd61dbd6efcc7668b910976964f1
plgmgua <plgmgua@gmail.com>
2025-10-11 22:29:47 -0600

    bump: version to 2.9.6-STABLE (simplified duplicate request)

--- com_ordenproduccion/src/View/Orden/HtmlView.php
+++ com_ordenproduccion/src/View/Orden/HtmlView.php
@@ -457,6 +457,6 @@ class HtmlView extends BaseHtmlView
     public function getComponentVersion()
     {
         // Hardcoded version - update this with each release
-        return '2.9.5-STABLE';
+        return '2.9.6-STABLE';
     }
 }
