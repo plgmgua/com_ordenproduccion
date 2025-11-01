43877729e2e75d68652fdf575139ec76b43eaa24
plgmgua <plgmgua@gmail.com>
2025-11-01 09:31:24 -0600

    Fix plugin manifest: add markdownrenderer.php entry file and update XML

---
+++ plg_content_markdownrenderer/markdownrenderer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package     Grimpsa\Plugin\Content\Markdownrenderer
+ * @subpackage  plg_content_markdownrenderer
+ *
+ * @copyright   (C) 2025 Grimpsa. All rights reserved.
+ * @license     GNU General Public License version 2 or later
+ */
+
+defined('_JEXEC') or die;
+
+// Require the main plugin class
+require_once __DIR__ . '/src/Extension/Markdownrenderer.php';
+
