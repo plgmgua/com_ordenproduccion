02db17429b37d711bd61e025f2742a7c8d33e309
plgmgua <plgmgua@gmail.com>
2026-01-19 09:08:03 -0600

    Simplify run.sh to match server version exactly

--- run.sh
+++ run.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# Remove old versions to prevent .1, .2, etc. extensions
-rm -f deploy_to_server.sh deploy_to_server.sh.* 2>/dev/null || true
-
-# Download the script
+rm deploy_to_server.sh
 wget https://raw.githubusercontent.com/plgmgua/com_ordenproduccion/refs/heads/main/deploy_to_server.sh
-
-# Make it executable
 chmod +x deploy_to_server.sh
-
-# Run the deployment script
 ./deploy_to_server.sh
